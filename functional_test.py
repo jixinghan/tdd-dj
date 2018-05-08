67b67d3b6fd53d30016f6ebe43918c20e732a5aa
jixinghan <hjxendeavor@gmail.com>
2018-05-08 23:34:09 +0900

    Front page HTML now generate from a template

--- functional_test.py
+++ functional_test.py
@@ -6,25 +6,25 @@ import time
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.broswer = webdriver.Chrome()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
-        self.broswer.quit()
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. she goes
         # to check out its homepage
-        self.broswer.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.broswer.title)
-        header_text = self.broswer.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            input_box.get_attribute('placeholder'), 'Enter a to-do list'
+            input_box.get_attribute('placeholder'), 'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into the text box (Edith's hobby
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item do not appear in table'
         )
 
         # There is still a  text box inviting her to add another item. She
