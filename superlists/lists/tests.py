f598e82a6209e9d0671ac4c392bfa5b1496cb6aa
jixinghan <hjxendeavor@gmail.com>
2018-05-11 18:12:19 +0900

    Redirect after POST request, and show all items in template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -10,11 +10,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
 
-    def test_can_manipulate_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
-
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -31,3 +26,29 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_only_saves_items_when_send_a_post_request(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_can_save_post_request_data_into_database(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        # Assume we have a set of items
+        item1 = Item.objects.create(text='item 1')
+        item2 = Item.objects.create(text='item 2')
+        # When client sends a request...
+        response = self.client.get('/')
+        # all these items should be in the response
+        self.assertIn(item1.text, response.content.decode())
+        self.assertIn(item2.text, response.content.decode())
